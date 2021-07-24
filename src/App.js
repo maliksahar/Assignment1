6a084d42a56c2f03c0cc26c2f9d1f409c9443e7b
maliksahar <ms7611453@gmail.com>
2021-07-25 01:37:09 +0500

    initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { Button } from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const  App=() =>{
+    const [count,SetCount] = useState(0);
+    
+     
+    return(
+    <div>
+    <center > 
+     <h1>Counter</h1>
+     <div  style={{ margin : "15%", width : "300" , height : "300" ,  padding :"5px" ,  justifyContent :"space-between"}}>
+     <h1>{count} </h1>
+    <Button  variant="success"    onClick={ ()=> SetCount(count + 1)}> Increment</Button>
+    
+    <Button  variant="danger"  onClick= { ()=> SetCount(count - 1)}> Decrement</Button>
+     </div>
+     </center>
     </div>
-  );
-}
+    )
+};
 
+ 
 export default App;
